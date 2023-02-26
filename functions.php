4a44add0667220d5dc3207916aee17f9863bae0f
Sébastien COLLET <seb.collet@gmail.com>
2023-02-26 15:33:52 +0100

    Suppression du code manuel de MATOMO

--- functions.php
+++ functions.php
@@ -91,22 +91,6 @@ add_action( 'wp_head', function() { ?>
         })(jQuery);
     </script>
 
-    <!-- Matomo -->
-    <script>
-    var _paq = window._paq = window._paq || [];
-    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
-    (function() {
-        var u="//stats.atelier-contigo.fr/";
-        _paq.push(['setTrackerUrl', u+'matomo.php']);
-        _paq.push(['setSiteId', '2']);
-        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-    })();
-    </script>
-    <!-- End Matomo Code -->
-
 <?php } );
 
 add_filter( 'gform_confirmation_anchor', '__return_true' );
