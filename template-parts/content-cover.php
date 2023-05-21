788ca411f2264d8ded891492c70d40acbbb3db96
Sébastien COLLET <seb.collet@gmail.com>
2023-05-21 15:16:24 +0200

    Mise en place du logo

--- template-parts/content-cover.php
+++ template-parts/content-cover.php
@@ -83,7 +83,11 @@
 								<?php
 							}
 
-							the_title( '<h1 class="entry-title">', '</h1>' );
+							//the_title( '<h1 class="entry-title">', '</h1>' );
+							// Mise en place du logo version blanche
+							?>
+							<img src="https://www.coupille.fr/wp-content/uploads/2023/05/paul_coupille_white_900.png" alt="" class="homeLogo"/>
+							<?php
 
 							if ( is_page() ) {
 								?>
@@ -167,22 +171,6 @@
 	if ( is_single() ) {
 
 		get_template_part( 'template-parts/navigation' );
-	}
-
-	/*
-	 * Output comments wrapper if it's a post, or if comments are open,
-	 * or if there's a comment number – and check for password.
-	 */
-	if ( ( is_single() || is_page() ) && ( comments_open() || get_comments_number() ) && ! post_password_required() ) {
-		?>
-
-		<div class="comments-wrapper section-inner">
-
-			<?php comments_template(); ?>
-
-		</div><!-- .comments-wrapper -->
-
-		<?php
 	}
 	?>
 
