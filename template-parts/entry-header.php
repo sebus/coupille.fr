d7adb3df35eaa8e35f878ae806d575d899b48d15
Sébastien COLLET <seb_collet@iMac-Sebus-Home.local>
2024-02-03 15:36:03 +0100

    Up css

--- template-parts/entry-header.php
+++ template-parts/entry-header.php
@@ -12,7 +12,9 @@ $entry_header_classes = '';
 if ( is_singular() ) {
 	$entry_header_classes .= ' header-footer-group';
 }
-
+if ( is_page() ) {
+	// Si c'est une page 
+}else{
 ?>
 
 <header class="entry-header has-text-align-center<?php echo esc_attr( $entry_header_classes ); ?>">
@@ -20,21 +22,13 @@ if ( is_singular() ) {
 	<div class="entry-header-inner section-inner medium">
 
 		<?php
-
-		if ( is_singular() ) {
-			if ( is_page() ) {
-				// Si c'est une page 
-			}else{
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			}
-			
-		} else {
-			//the_title( '<h2 class="entry-title heading-size-1"><a href="' . esc_url( get_permalink() ) . '">', '</a></h2>' );
-		}
-
-
+			the_title( '<h1 class="entry-title">', '</h1>' );
 		?>
 
 	</div><!-- .entry-header-inner -->
 
 </header><!-- .entry-header -->
+
+<?php
+
+}
--- template-parts/toile-featured-image.php
+++ template-parts/toile-featured-image.php
@@ -14,49 +14,46 @@ if ( has_post_thumbnail() ) {
 	?>
 	<div class="toile-container">
 		<div class="toile-notice">
-			<table class="sticky">
-				<tr>
-					<td colspan="2"><i class="at-arrow-left"></i> <a href="#" onclick="history.go(-1)">Retourner au catalogue</a></td>
-				</tr>
-				<tr>
-					<td colspan="2"><?php the_post_thumbnail('medium');	?></td>
-				</tr>
-				<tr>
-					<td class="libelle">Titre</td>
-					<td><?php if(get_field( 'titre' )){the_field('titre');} ?></td>
-				</tr>
-				<tr>
-					<td class="libelle">Numéro de toile</td>
-					<td><?php if(get_field( 'num_toile' )){the_field('num_toile');} ?></td>
-				</tr>
-				<tr>
-					<td class="libelle">Année</td>
-					<td><?php if(get_field( 'annee' )){the_field('annee');} ?></td>
-				</tr>
-				<tr>
-					<td class="libelle">Dimensions</td>
-					<td><?php if(get_field( 'dimensions' )){echo str_replace('X',' x ',get_field('dimensions'));} ?></td>
-				</tr>
-				<tr>
-					<td class="libelle">Collection</td>
-					<td><?php 
-					if(get_field( 'collection' ))
-					{
-						$field = get_field_object( 'collection' );
-						$value = $field['value'];
-						$label = $field['choices'][ $value ];
+			<div class="sticky">
+				<!--<p><i class="at-arrow-left"></i> <a href="#" onclick="history.go(-1)">Retourner au catalogue</a></p>-->
+				<?php the_post_thumbnail('medium');	?></td>
+					
+				<table>
+					<tr>
+						<td class="libelle">Titre</td>
+						<td><?php if(get_field( 'titre' )){the_field('titre');} ?></td>
+					</tr>
+					<tr>
+						<td class="libelle">Numéro de toile</td>
+						<td><?php if(get_field( 'num_toile' )){the_field('num_toile');} ?></td>
+					</tr>
+					<tr>
+						<td class="libelle">Année</td>
+						<td><?php if(get_field( 'annee' )){the_field('annee');} ?></td>
+					</tr>
+					<tr>
+						<td class="libelle">Dimensions</td>
+						<td><?php if(get_field( 'dimensions' )){echo str_replace('X',' x ',get_field('dimensions'));} ?></td>
+					</tr>
+					<tr>
+						<td class="libelle">Collection</td>
+						<td><?php 
+						if(get_field( 'collection' ))
+						{
+							$field = get_field_object( 'collection' );
+							$value = $field['value'];
+							$label = $field['choices'][ $value ];
 
-						echo $label;
-					}
-						
-						
-					?></td>
-				</tr>
-				<tr>
-					<td colspan="2"><i class="at-envelope-question"></i> <a href="<?php echo the_permalink( 9 ) ?>?objet=<?php the_title(); ?>">Poser une question</a></td>
-				</tr>
-				
-			</table>
+							echo $label;
+						}
+							
+							
+						?></td>
+					</tr>
+				</table>
+
+				<p><i class="at-envelope-question"></i> <a href="<?php echo the_permalink( 9 ) ?>?objet=<?php the_title(); ?>">Poser une question</a></p>
+			</div>
 
 		</div>
 
