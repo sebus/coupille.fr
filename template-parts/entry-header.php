33549052c40b6872756140364c0bc9c6e5b1deb4
Sébastien <seb_collet@iMac-de-Sebastien.local>
2025-02-16 19:06:09 +0100

    Mise à jour journal : fil d'ariane et navigation

--- template-parts/entry-header.php
+++ template-parts/entry-header.php
@@ -25,6 +25,28 @@ if ( is_page() ) {
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		?>
 
+<?php
+// Fonction personnalisée pour afficher le fil d'Ariane
+function custom_breadcrumbs() {
+    echo '<nav class="breadcrumb">';
+    echo '<a href="' . home_url() . '">Accueil</a> » ';
+    
+    if (is_single()) {
+        // $category = get_the_category();
+        // if (!empty($category)) {
+        //     echo '<a href="' . get_category_link($category[0]->term_id) . '">' . $category[0]->name . '</a> » ';
+        // }
+		echo '<a href="https://www.coupille.fr/journal/">Journal</a> » ';
+        echo '<span>' . get_the_title() . '</span>';
+    }
+
+    echo '</nav>';
+}
+
+// Afficher le fil d'Ariane
+custom_breadcrumbs();
+?>
+
 	</div><!-- .entry-header-inner -->
 
 </header><!-- .entry-header -->
